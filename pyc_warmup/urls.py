b5855cb1b8abb218781a2bc6a719b3ae67d5f1c6
elishambadi <embadi43@gmail.com>
2025-02-18 14:41:21 +0300

    reorder urls

--- pyc_warmup/urls.py
+++ pyc_warmup/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 import pwa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('serviceworker.js', TemplateView.as_view(template_name="serviceworker.js", content_type='application/javascript')),
     path('', include('songs.urls')),
     path('', include('pwa.urls')),
 ]
